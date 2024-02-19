68b84e602de9ff81cc9f2ae61f98410ccc24b4e4
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-02-19 18:18:02 +0100

    added new addresses

--- scripts/Dex/deployPool.js
+++ scripts/Dex/deployPool.js
@@ -3,19 +3,51 @@ const { ethers, getNamedAccounts } = require("hardhat");
 async function deployPool() {
   console.log("Connecting to the contracts...");
   const { deployer } = await getNamedAccounts();
+  const accounts = await ethers.getSigners();
+  console.log(accounts[0]);
+  console.log(deployer);
+  // const poolTracker = await ethers.getContract(
+  //   "PoolTracker",
+  //   "0x1044E8b79B873f09Dc2c0665532c267D470c8E0c",
+  //   accounts[0].address
+  // );
   const poolTracker = await ethers.getContract("PoolTracker", deployer);
   const token1 = await ethers.getContract("TestToken1", deployer);
   const token2 = await ethers.getContract("TestToken2", deployer);
+  console.log(token1.target);
+  console.log(token2.target);
   console.log("Connected to the contract!");
 
   console.log("Deploying the pool...");
-  await token1.approve(poolTracker.target, ethers.parseEther("100"));
-  await token2.approve(poolTracker.target, ethers.parseEther("100"));
+  console.log("Approving token...");
+  console.log(poolTracker.target);
+  await token1.approve(poolTracker.target, ethers.parseEther("1000"));
+  await token2.approve(poolTracker.target, ethers.parseEther("1000"));
+  console.log(
+    `This is the deployer token1 balance ${await token1.balanceOf(deployer)}`
+  );
+  console.log(
+    `This is the deployer token2 balance ${await token2.balanceOf(deployer)}`
+  );
+  console.log(
+    `This is the deployer token1 allowance ${await token1.allowance(
+      deployer,
+      poolTracker.target
+    )}`
+  );
+  console.log(
+    `This is the deployer token2 allowance ${await token2.allowance(
+      deployer,
+      poolTracker.target
+    )}`
+  );
+  console.log("Tokens approved!");
+  console.log("Creating Pool...");
   await poolTracker.createPool(
     token1.target,
     token2.target,
-    ethers.parseEther("100"),
-    ethers.parseEther("100")
+    ethers.parseEther("10"),
+    ethers.parseEther("10")
   );
   console.log("Pool deployed!");
 }
